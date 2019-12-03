54295b463b18fab5dec9a7ac37d9fbd709b5476f
Barbara <muesingb@gmail.com>
2019-12-03 16:57:45 -0500

    sidebar and working on endgame

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 let moleImageURL = "https://image.shutterstock.com/image-vector/cartoon-cute-mole-260nw-274323440.jpg"
 let moleCounter = 0
+let container = document.querySelector('.container')
+
+let scoreHTML = document.querySelector('#score')
+let scoreCounter = 0
 
 fetch('http://localhost:3000')
   .then(response => response.json())
@@ -7,8 +11,16 @@ fetch('http://localhost:3000')
     // console.log(data)
   })
 
-//mole timing
-setInterval(function(){ molePopUp() }, 3000);
+//start game
+function startGame() {
+  scoreHTML.innerHTML = "0"
+  
+  setInterval(function(){ molePopUp() }, 1000); //mole timing
+
+  //end game
+      //render end game pop-up
+      //send post request with users name and score to backend
+};
 
 function molePopUp() {
   if (document.querySelector('.mole')) {
@@ -36,3 +48,18 @@ function randomHole() {
   let holeID = Math.floor((Math.random() * 9) + 1)
   return document.querySelector(`[data-id="${holeID}"]`)
 }
+
+//listen for mole click
+container.addEventListener('click', function(event) {
+  if (event.target.className === "mole") {
+    scoreCounter += 1
+    scoreHTML.innerHTML = parseInt(scoreCounter) + 1
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  if (moleCounter >= 10) {
+    clearInterval()
+    moleCounter = 0
+  }
+})

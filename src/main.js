5d6878206829756e9ba908e6067e68d15a6c2cce
Barbara <muesingb@gmail.com>
2019-12-03 15:35:51 -0500

    mole timing and popup. Added CSS help

--- src/main.js
+++ src/main.js
@@ -1,5 +1,38 @@
+let moleImageURL = "https://image.shutterstock.com/image-vector/cartoon-cute-mole-260nw-274323440.jpg"
+let moleCounter = 0
+
 fetch('http://localhost:3000')
   .then(response => response.json())
   .then(data => {
     // console.log(data)
   })
+
+//mole timing
+setInterval(function(){ molePopUp() }, 3000);
+
+function molePopUp() {
+  if (document.querySelector('.mole')) {
+    document.querySelector('.mole').remove()
+    renderMole()
+  } else {
+    renderMole()
+    }
+}
+
+//mole render
+function renderMole() {
+  randomHole()
+  
+  let moleHTML = `
+    <img class="mole" src=${moleImageURL}>
+    `
+    randomHole().insertAdjacentHTML('beforeend',moleHTML)
+
+    moleCounter += 1
+}
+
+//pick a hole
+function randomHole() {
+  let holeID = Math.floor((Math.random() * 9) + 1)
+  return document.querySelector(`[data-id="${holeID}"]`)
+}

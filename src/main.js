0239055cf3542bdbeb8ce8d7564eec82a76bec13
Barbara <muesingb@gmail.com>
2019-12-04 11:28:50 -0500

    created game timer, moles speed up every 5 points, and start button toggles

--- src/main.js
+++ src/main.js
@@ -1,45 +1,63 @@
 let moleImageURL = "https://image.shutterstock.com/image-vector/cartoon-cute-mole-260nw-274323440.jpg"
 let moleCounter = 0
-let score = document.querySelector("#score")
+let scoreSelector = document.querySelector("#score")
 let container = document.querySelector(".container")
-let clickable = false
-let timer = 0
+let clickableMole = false
 let startButton = document.querySelector('#start-button')
+let timer = 30
+let countdownTimer = document.querySelector('#countdown-timer')
 
 fetch('http://localhost:3000')
   .then(response => response.json())
   .then(data => {
     console.log(data)
-  })
+  });
 
 //start game
+let timerInterval;
 let interval;
 function startGame() {
-  score.textContent = "0"
-  interval = setInterval(function(){ molePopUp() }, 1000);//mole timing
+  scoreSelector.textContent = "0"
+  interval = setInterval(function(){ molePopUp() }, 3000);//mole timing
+  timerInterval = setInterval(function(){ timerCountdown() }, 1000);// game countdown
 };
 
+//
 function molePopUp() {
-  //can do Date.now() and see what difference is
-  if (timer < 5) {
-    if (document.querySelector('.mole')) {
-      removeMole()
-      renderMole()
-      } else {
+  //make a function to add random photos
+  if (timer > 0) {
+      if (document.querySelector('.mole')) {
+        removeMole()
         renderMole()
-      }
-      timer++
+        } else {
+          renderMole()
+        }
+      determineSpeed()
   } else {
       endGame()
   }
 };
 
+//timer countdown
+function timerCountdown() {
+      let newTime;
+      if (timer >= 10) {
+        newTime = ("00:" + timer.toString())
+      } else if (timer >= 0) {
+        newTime = ("00:0" + timer.toString())
+      }
+      countdownTimer.innerText = newTime
+      timer--
+};
+
 //end game function
 function endGame() {
-  timer = 0
+  timer = 30
+  clearInterval(timerInterval)
   clearInterval(interval)
   if (document.querySelector('.mole')) {removeMole()}
-  console.log(score)
+  toggleStartButton()
+  console.log(parseInt(scoreSelector.textContent))
   //end game
     //render end game screen
     //send post/patch request to back end
@@ -47,7 +65,7 @@ function endGame() {
 
 //mole render
 function renderMole() {
-  clickable = true
+  clickableMole = true
   randomHole()
   
   let moleHTML = `
@@ -56,30 +74,50 @@ function renderMole() {
     randomHole().insertAdjacentHTML('beforeend', moleHTML)
 
     moleCounter += 1
-}
+};
 
 //pick a hole
 function randomHole() {
   let holeID = Math.floor((Math.random() * 9) + 1)
   return document.querySelector(`[data-id="${holeID}"]`)
-}
+};
 
 //listen for mole click
 container.addEventListener("click", function(e) {
-
-  if (e.target.className === "mole" && clickable) {
-    score.innerHTML = parseInt(score.textContent) + 1
+  if (e.target.className === "mole" && clickableMole) {
+    scoreSelector.innerHTML = parseInt(scoreSelector.textContent) + 1
     removeMole()
-    clickable = false
+    clickableMole = false
   }
-})
+});
 
 //event listener for start game button
 startButton.addEventListener('click', function(event) {
   startGame()
+  toggleStartButton()
 });
+
+//function to show or hide start button
+function toggleStartButton() {
+  if (startButton.style.display === "none") {
+    startButton.style.display = "block";
+  } else {
+    startButton.style.display = "none";
+  }
+};
 
 //helper function remove mole
 function removeMole() {
   document.querySelector('.mole').remove()
+};
+
+//make timer faster depending on score
+function determineSpeed() {
+  if (parseInt(scoreSelector.textContent) == 5) {
+    clearInterval(interval)
+    interval = setInterval(function(){ molePopUp() }, 2000);
+  } else if (parseInt(scoreSelector.textContent) == 10) {
+    clearInterval(interval)
+    interval = setInterval(function(){ molePopUp() }, 1000);
+  }
 };
